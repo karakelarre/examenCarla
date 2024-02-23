5b98e3f9e10b591597bd9df29b9926418b02a321
karakelarre <nrx54524@educastur.es>
2024-02-23 19:02:59 +0100

    Añadidos los primeros ficheros al proyecto.

---
+++ src/miExamen/ClaseExamen.java
@@ -0,0 +1,19 @@
+
+package miExamen;
+
+/**
+ *
+ * @author Carla
+ */
+public class ClaseExamen {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        System.out.println("Estoy haciendo el examen de la UT3");
+        
+    }
+    
+}
