ab8584cef2dc2b144bb5f9da5515b28709b22236
karakelarre <nrx54524@educastur.es>
2024-02-23 19:11:29 +0100

    Añadidos comentarios de documentacion de claseExamen

--- src/miExamen/ClaseExamen.java
+++ src/miExamen/ClaseExamen.java
@@ -3,7 +3,13 @@ package miExamen;
 
 /**
  *
+ * <h1>Entornos de desarrollo UT3</h1>;
+ * <p>Esta clase sirve para realizar el examen de la UT3 del módulo Entornos de Desarrollo</p>
+ * @see <a href="https://www.youtube.com/playlist?list=PLU8oAlHdN5BlyaPFiNQcV0xDqy0eR35aU"/>
+ * 
  * @author Carla
+ * @version 1.0.0
+ * @since 23/02/2024
  */
 public class ClaseExamen {
 
@@ -13,6 +19,7 @@ public class ClaseExamen {
     public static void main(String[] args) {
         
         System.out.println("Estoy haciendo el examen de la UT3");
+        
         
     }
     
